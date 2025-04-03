a2e83def865b1e861a0b17c52e17400e81c44002
PaulAllen000 <152220087+PaulAllen000@users.noreply.github.com>
2025-04-03 10:46:55 +0100

    Create jest.config.js

---
+++ scrum-ui/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/'
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+  },
+};
