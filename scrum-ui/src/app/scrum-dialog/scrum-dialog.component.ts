143c65e0931bd9bc02dcbe0998a60553c2083b02
PaulAllen000 <152220087+PaulAllen000@users.noreply.github.com>
2025-05-15 10:51:22 +0100

    Update scrum-dialog.component.ts

--- scrum-ui/src/app/scrum-dialog/scrum-dialog.component.ts
+++ scrum-ui/src/app/scrum-dialog/scrum-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { ScrumService } from '../service/scrum-service.service';
 import { Scrum } from '../model/scrum/scrum';
